af4ac1d14164e9666387d0e67bfb737d55edc17c
Valderio13 <valderio_jr@hotmail.com>
2022-08-28 22:20:07 -0300

    Criando connection string

--- Rest/Rest/Startup.cs
+++ Rest/Rest/Startup.cs
@@ -1,16 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Rest.Model.Context;
 using Rest.Services;
 using Rest.Services.Implemetations;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Rest
 {
@@ -28,6 +24,9 @@ namespace Rest
         {
 
             services.AddControllers();
+
+            var connection = Configuration["MySQLConnection:MySQLConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
 
             services.AddScoped<IPersonService, PersonServiceImplementation>();
         }
