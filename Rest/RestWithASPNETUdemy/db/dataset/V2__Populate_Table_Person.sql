47054148b74af40c5c5fb6abde9167c55f625155
Valderio13 <valderio_jr@hotmail.com>
2022-08-31 18:02:09 -0300

    Criando Swagger na API

---
+++ Rest/RestWithASPNETUdemy/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`,`gender`) VALUES
+	(1,'Valderio', 'Junior', 'São Paulo - SP - Brasil', 'Male'),
+	(2,'Anielle', 'Viana', 'Ribeirão Preto - SP - Brasil', 'Female');
